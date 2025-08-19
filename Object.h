4f9aab8220ed042bfed8b27e3fedafe7f2db902a
wangjing <wangjing@outloolk.com>
2025-08-19 14:54:51 +0800

    功能：测试异常类 能正确输出异常信息(文件和行数)

---
+++ Object.h
@@ -0,0 +1,25 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <cstdlib>
+
+namespace WLib
+{
+class Object
+{
+public:
+		void* operator new(size_t size) throw();
+		void operator delete(void*p);
+		void* operator new[](size_t size) throw();
+		void operator delete[](void*p);
+
+		bool operator ==(const Object& obj);
+		bool operator !=(const Object& obj);
+
+		virtual ~Object()=0;
+};
+}
+
+
+
+#endif
