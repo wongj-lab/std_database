6b25e3cadf6204a0b2ca776b0dd7408ff3626a4c
wangjing <wangjing@outloolk.com>
2025-08-19 16:18:26 +0800

    新增智能指针测试

---
+++ Pointer.h
@@ -0,0 +1,60 @@
+#ifndef POINTER_H
+#define POINTER_H
+
+#include "Object.h"
+
+namespace WLib
+{
+template <typename T>
+class Pointer:public Object
+{
+	protected:
+		T* m_pointer;
+	public:
+		Pointer(T* p=NULL)
+		{
+			m_pointer = p;
+		}
+
+		T* operator->()
+		{
+			return m_pointer;
+		}
+
+		const T& operator*()const
+		{
+			return *m_pointer;
+		}
+
+		const T* operator->()const
+		{
+			return m_pointer;
+		}
+
+		T& operator*()
+		{
+			return *m_pointer;
+		}
+
+		bool isNull()const
+		{
+			return (m_pointer == NULL);
+		}
+
+		T* get() const
+		{
+			return m_pointer;
+		}
+
+		~Pointer() = 0;
+};
+
+template <typename T>
+Pointer<T>::~Pointer()
+{
+
+}
+
+}
+#endif
+
