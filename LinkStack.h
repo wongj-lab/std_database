7c822dbc6089bfc3987f86adfc2e73ea328be5be
wangjing <wangjing@outloolk.com>
2025-08-27 09:41:35 +0800

    添加链式栈代码以及测试demo

---
+++ LinkStack.h
@@ -0,0 +1,59 @@
+#ifndef LINKSTACK_H
+#define LINKSTACK_H
+
+#include "Stack.h"
+#include "LinkList.h"
+
+namespace WLib
+{
+template <typename T>
+class LinkStack : public Stack<T>
+{
+protected:
+	LinkList<T> m_list;
+public:
+	void push(const T& e)
+	{
+		m_list.insert(0,e);
+	}
+
+	void pop()
+	{
+		if(m_list.length()>0)
+		{
+			m_list.remove(0);
+		}
+		else
+		{
+			THROW_EXCEPTION(InvalidOperationException,"no element in current stack....");
+		}
+	}
+
+	T top()const
+	{
+		if(m_list.length())
+		{
+			return m_list.get(0);
+		}
+		else
+		{
+			THROW_EXCEPTION(InvalidOperationException," no element in current stack ...");
+		}
+	}
+
+	void clear()
+	{
+		m_list.clear();
+	}
+
+	int size()const
+	{
+		return m_list.length();
+	}
+};
+}
+
+
+
+#endif
+
